b3228ba573f35b774da13b6df73c5331da981e06
MCarmen <carmengarcianavarro19@gmail.com>
2025-10-09 20:43:22 +0200

    funcionActualizar

--- dwec_t03p03_garcia_mcarmen/js/ejercicio10.js
+++ dwec_t03p03_garcia_mcarmen/js/ejercicio10.js
@@ -1,39 +1,39 @@
 console.log("T03 P03 - Ejercicio 10");
 
 const datosEquipos = [
-                        ["Equipo","pts","pj","pg","pe","pp"],
-                        ["Levante",40, 14, 13, 1, 0], 
-                        ["Malaga",37, 14, 12, 1, 1], 
-                        ["Eibar",34, 14, 11, 1, 2], 
-                        ["Cordoba",27, 14, 8, 3, 3] ];
+    ["Equipo", "pts", "pj", "pg", "pe", "pp"],
+    ["Levante", 40, 14, 13, 1, 0],
+    ["Malaga", 37, 14, 12, 1, 1],
+    ["Eibar", 34, 14, 11, 1, 2],
+    ["Cordoba", 27, 14, 8, 3, 3]];
 let opcion;
 
 
 function equipoLider(matriz) {
 
-    let puntosMaximo=0;
+    let puntosMaximo = 0;
     let lider;
 
     for (let i = 1; i < matriz.length; i++) {
 
-        if(matriz[i][1]>puntosMaximo){
-            puntosMaximo=matriz[i][1];
-            lider=matriz[i][0];
+        if (matriz[i][1] > puntosMaximo) {
+            puntosMaximo = matriz[i][1];
+            lider = matriz[i][0];
         }
     }
     return lider;
 
 }
 
-function equipoConMasPartidosPerdidos(matriz){
+function equipoConMasPartidosPerdidos(matriz) {
 
-    let partidosPerdidos=matriz[1][5];
-    let NombreEquipo=matriz[1][0];
-    
-    for (let index =1; index < matriz.length; index++) {
-        
-        if(matriz[index][5]>partidosPerdidos){
-            NombreEquipo=matriz[index][0];
+    let partidosPerdidos = matriz[1][5];
+    let NombreEquipo = matriz[1][0];
+
+    for (let index = 1; index < matriz.length; index++) {
+
+        if (matriz[index][5] > partidosPerdidos) {
+            NombreEquipo = matriz[index][0];
         }
     }
 
@@ -41,75 +41,151 @@ function equipoConMasPartidosPerdidos(matriz){
 
 }
 
-function equipoConMasPartidosGanados(matriz){
-    let partidosGanados=matriz[1][3];
-    let NombreEquipo=matriz[1][0];
+function equipoConMasPartidosGanados(matriz) {
+    let partidosGanados = matriz[1][3];
+    let NombreEquipo = matriz[1][0];
 
     for (let index = 0; index < matriz.length; index++) {
-        
-        if(matriz[index][3]>partidosGanados){
-            NombreEquipo=matriz[index][0];
+
+        if (matriz[index][3] > partidosGanados) {
+            NombreEquipo = matriz[index][0];
         }
     }
 
     return NombreEquipo;
 }
 
-function mostrarClasificacion(matriz){
-    
+function mostrarClasificacion(matriz) {
+
     console.log("--CLASIFICACION--");
     console.table(matriz);
 
 }
 
-function añadirEquipo(){
-    let nombre=prompt("Nombre del equipo: ");
-    let partidosGanados=Number(prompt("Partidos ganados: "));
-    let partidosPerdidos=Number(prompt("Partidos perdidos: "));
-    let partidosEmpatados=Number(prompt("Partidos empanados: "));
+function añadirEquipo(matriz) {
 
-    let partidosJugados=partidosGanados+partidosEmpatados+partidosPerdidos;
-    let puntos=0;
+    let nombre;
+    let partidosGanados;
+    let partidosPerdidos;
+    let partidosEmpatados;
+    let partidosJugados;
 
-    if(partidosGanados>0){
-        puntos+=3;
-    }else{
-        
+    do {
+        nombre = prompt("Nombre del equipo: ");
+        partidosGanados = Number(prompt("Partidos ganados: "));
+        partidosPerdidos = Number(prompt("Partidos perdidos: "));
+        partidosEmpatados = Number(prompt("Partidos empatados: "));
+
+        partidosJugados = partidosGanados + partidosEmpatados + partidosPerdidos;
+
+    } while (partidosJugados > matriz[1][2] || nombre == "" || partidosGanados == "" || partidosPerdidos == "" || partidosEmpatados == "");
+
+    let puntosTotales = 0;
+
+
+    if (partidosGanados > 0) {
+        puntosTotales += (3 * partidosGanados);
     }
 
+    if (partidosEmpatados > 0) {
+        puntosTotales += partidosEmpatados;
+    }
 
-    
+    let nuevoEquipo = [nombre, puntosTotales, partidosJugados, partidosGanados, partidosPerdidos, partidosPerdidos];
+
+    matriz.push(nuevoEquipo);
+
+    console.log(`--CLASIFICACION CON NUEVO EQUIPO-> ${nombre}`);
+    console.table(matriz);
+    return matriz;
 }
 
+function actualizarDatos(matriz) {
 
-console.table(datosEquipos);
+    let nombreEqp;
+    let prtdGanados;
+    let prtdPerdido;
+    let prtdEmpatado;
+    let prtJugados = 0;
+    let puntosTotales=0;
+
+    do {
+
+        nombreEqp = prompt("Introduce el nombre del equipo:");
+
+    } while (nombreEqp == "");
+
+    for (let index = 1; index < matriz.length; index++) {
+
+        if (nombreEqp == matriz[index][0]) {
+
+            console.log("Equipo encontrado" + nombreEqp);
+            prtdGanados = Number(prompt("Partidos ganados:"));
+            prtdEmpatado = Number(prompt("Partidos emepatados:"));
+            prtdPerdido = Number(prompt("Partidos empatados:"));
+
+            prtJugados = prtdGanados + prtdEmpatado + prtdPerdido;
+
+            if(prtdGanados>0){
+                puntosTotales+=3*prtdGanados;
+            }else{
+                console.log("No ha ganado ningun partido");
+            }
+
+            if(prtdEmpatado>0){
+                puntosTotales+=prtdEmpatado;
+            }else{
+                console.log("No ha empatado ningun partido");
+            }
+
+
+            matriz[index][1]+=puntosTotales;
+            matriz[index][2]+=prtJugados;
+            matriz[index][3]+=prtdGanados;
+            matriz[index][4]+=prtdEmpatado;
+            matriz[index][5]+=prtdPerdido;
+
+
+        } else {
+            console.log("El equipo introducido no esta en la clasificacion");
+        }
+    }
+
+    return matriz;
+
+}
+
+//console.table(datosEquipos);
 
 do {
 
-    opcion = Number(prompt("--MENU--\n 1.Equipo lider clasifiacion\n 2.Equipo con mas partidos perdidos\n 3.Equipo con mas partidos ganados\n 4.Dime todo\n 0.Salir"));
+    opcion = Number(prompt("MENU\n1.Equipo lider\n2.Equipo partidos perdidos\n3.Equipos partidos ganados\n4.Mostrar clasificacion\n5.Añadir equipo\n0.Salir"));
 
     switch (opcion) {
         case 1:
-            
+
             console.log("Equipo lider: " + equipoLider(datosEquipos));
             break;
         case 2:
-            console.log("Equipo con mas partidos perdidos: "+equipoConMasPartidosPerdidos(datosEquipos));
+            console.log("Equipo con mas partidos perdidos: " + equipoConMasPartidosPerdidos(datosEquipos));
             break;
         case 3:
-            console.log("Equipo con mas partidos ganados: "+equipoConMasPartidosGanados(datosEquipos));
+            console.log("Equipo con mas partidos ganados: " + equipoConMasPartidosGanados(datosEquipos));
             break;
+        //case 4:
+        //console.log("Lider: "+equipoLider(datosEquipos)+"\n Equipo con mas partidos ganados: "+equipoConMasPartidosGanados(datosEquipos)+"\n quipo con mas partidos perdidos: "+equipoConMasPartidosPerdidos(datosEquipos));
+        //break;
         case 4:
-            console.log("Lider: "+equipoLider(datosEquipos)+"\n Equipo con mas partidos ganados: "+equipoConMasPartidosGanados(datosEquipos)+"\n quipo con mas partidos perdidos: "+equipoConMasPartidosPerdidos(datosEquipos));
+            console.log("Clasificacion: " + mostrarClasificacion(datosEquipos));
             break;
         case 5:
-            console.log("Clasificacion: "+mostrarClasificacion(datosEquipos));
-            break;
+            console.log(añadirEquipo(datosEquipos));
+
         default:
             console.log("No valida");
     }
 
 
-} while (opcion<0||opcion>5);
+} while (opcion < 0 || opcion > 5);
 
 
